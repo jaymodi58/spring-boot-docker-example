56b924ad10bd30ac6219df5337dd421eea86488b
LAPTOP-3NA0PAG2\Jay Modi <jay.modi58@gmail.com>
2018-08-22 21:52:29 -0400

    Added the source code of the service.

---
+++ addition-service/src/main/java/com/calculator/additionservice/AdditionController.java
@@ -0,0 +1,14 @@
+package com.calculator.additionservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdditionController {
+
+	@GetMapping("/add/{a}/{b}")
+	public int add(@PathVariable int a, @PathVariable int b) {
+		return a + b;
+	}
+}
